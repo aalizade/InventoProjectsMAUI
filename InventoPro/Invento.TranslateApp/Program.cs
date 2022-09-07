50947fa31027f49d2ceffcc36dc2b0d3035d080f
alizade <agha.alizade@milko.com.tr>
2022-09-07 16:19:16 +0300

    Uygulamalar oluşturuldu

---
+++ InventoPro/Invento.TranslateApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string tercumeEdilecekDosyasi = @"C:\\Users\\agha_\\Desktop\\Invento\\InventoProjectsMAUI\\InventoPro\\core.json";
+Console.WriteLine("Hello, World!");
+
+TercumeyiBaslat();
+
+void TercumeyiBaslat()
+{
+    Console.WriteLine("Merhaba");
+}
+
+/*
+ * Her reguestten sonra 2 saniye beklenecek. Tercüme edemediği kısımlarda boş geçececek. Sonuçları json olarak dosyaya yazacak.
+ https://web-api.itranslateapp.com/v3/texts/translate
+{"source": {"dialect": "tr", "text": "Ben öğrenciyim", "with": ["synonyms"]}, "target": {"dialect": "en-UK"}}
+
+ */
+Console.ReadLine();
+
---
+++ InventoPro/Invento.XalqNagillari/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Invento.XalqNagillari
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
