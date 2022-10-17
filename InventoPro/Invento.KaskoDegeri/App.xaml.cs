08efe0d6616020833536fecc6e2b1463b3f36b1c
alizade <agha.alizade@milko.com.tr>
2022-10-17 15:04:39 +0300

    Kasko değeri

---
+++ InventoPro/Invento.KaskoDegeri/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Invento.KaskoDegeri
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ InventoPro/Invento.KaskoDegeri/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Invento.KaskoDegeri
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
